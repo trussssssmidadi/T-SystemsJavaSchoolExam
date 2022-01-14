43285ca14fb225bbe82354f10da1935250e2e36c
Valerii <zzzhugaru@gmail.com>
2022-01-14 18:15:58 +0300

    pyramid building (1 version)

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,32 +1,30 @@
 package main.java.com.tsystems.javaschool.tasks.pyramid;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PyramidBuilder {
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
-        if (inputNumbers.contains(null)) {
-            throw new CannotBuildPyramidException();
-        }
+    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
+        int size = inputNumbers.size();
+        int index = 0;
+        int row = numberOfRows(size);
+        int col = 2 * row - 1;
+        int[][] result = new int[row][col];
+
         Collections.sort(inputNumbers);
 
-        return new int[0][0];
+        for (int i = 0; i < row; i++) {
+            for (int j = row - i - 1; j < row + i; j += 2) {
+                result[i][j] = inputNumbers.get(index++);
+            }
+        }
+        return result;
     }
-    private static int numberOfRows(int size) {
+    private static int numberOfRows(int size) throws CannotBuildPyramidException {
         double result = (Math.sqrt(1 + 8 * size) - 1) / 2;
 
         if (result == Math.ceil(result)) {
             return (int) result;
-
-        } else {
-            throw new CannotBuildPyramidException();
         }
-    }
-
-    public static void main(String[] args) {
-        PyramidBuilder p = new PyramidBuilder();
-        List<Integer> input = List.of(1,9,3,6,4,2);
-        p.buildPyramid(input);
+        return 0;
     }
 }

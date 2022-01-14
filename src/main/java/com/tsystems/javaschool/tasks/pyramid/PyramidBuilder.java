d54426b36d3f086feccab937541608595d4c9509
Valerii <zzzhugaru@gmail.com>
2022-01-14 20:42:18 +0300

    numberOfRows upd3

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -3,17 +3,19 @@ package main.java.com.tsystems.javaschool.tasks.pyramid;
 import java.util.*;
 
 public class PyramidBuilder {
-    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
-        int size = inputNumbers.size();
-        int row = numberOfRows(size);
+    public int[][] buildPyramid(List<Integer> inputNumbers) {
+        if (inputNumbers.contains(null)) {
+            throw new CannotBuildPyramidException();
+        }
+        int index = 0;
+        int row = numberOfRows(inputNumbers.size());
         int col = 2 * row - 1;
         int[][] result = new int[row][col];
 
         Collections.sort(inputNumbers);
 
-        for (int i = 0; i < row; i++) {
-            int index = 0;
-            for (int j = row - i - 1; j < row + i; j += 2) {
+        for(int i = 0; i < row; i++) {
+            for(int j = row - i - 1; j < row + i; j += 2) {
                 result[i][j] = inputNumbers.get(index++);
             }
         }

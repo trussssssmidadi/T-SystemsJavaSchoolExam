4bf5ddc0fe56322c316b1272041eafd21aa1fc89
Valerii <zzzhugaru@gmail.com>
2022-01-14 22:46:47 +0300

    subsequence upd4

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,21 +1,19 @@
 package main.java.com.tsystems.javaschool.tasks.subsequence;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Subsequence {
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
+        int countX = 0;
         if (x == null || y == null) {
             throw new IllegalArgumentException();
         }
-
-        for(int i = 0; i < y.size(); i++) {
-            if (!y.contains(y.get(i)))
-                return false;
+        for (Object o : y) {
+            if (o.equals(x.get(countX)) && ++countX == x.size()) {
+                return true;
+            }
         }
-        return true;
+        return false;
     }
 }

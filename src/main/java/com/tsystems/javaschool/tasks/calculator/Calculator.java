b127031312f0552c1cfdc9c2e1d23d2d9f68f504
Valerii <zzzhugaru@gmail.com>
2022-01-14 15:45:14 +0300

    creating a list of separate tokens

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,2 +1,11 @@
+package main.java.com.tsystems.javaschool.tasks.calculator;
+
+import java.util.List;
+
 public class Calculator {
+    public String evaluate(String statement) {
+        List<String> tokens = List.of(statement.split("(?<=[-+*/])|(?=[-+*/])"));
+        String operators = "+-/*";
+        return "";
+    }
 }

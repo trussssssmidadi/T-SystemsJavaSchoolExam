005ff8b027742729c7806f4fa74cf71a891622ff
Valerii <zzzhugaru@gmail.com>
2022-01-14 21:29:35 +0300

    subsequence upd

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,15 +1,24 @@
 package main.java.com.tsystems.javaschool.tasks.subsequence;
 
-
 import java.util.List;
 
 public class Subsequence {
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
-        if (x.contains(y)) {
-            return true;
-        } else {
-            return false;
+        try {
+            if ((x == null && y != null) || (x != null && y == null)) {
+                return false;
+
+            } else if (x == null || x.isEmpty() && y.isEmpty()) {
+                return true;
+
+            } else if (x.size() != y.size()) {
+                return false;
+            }
+            return x.equals(y);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
         }
     }
 }
